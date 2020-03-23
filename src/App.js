ac78405ea8074ca2955fd76807a8f6f9c3196640
xeusteerapat <xeus085@gmail.com>
2020-03-23 20:48:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  let point;
+  let rows = [];
+
+  while (true) {
+    point = Number(prompt('Enter your point'));
+    if (isNaN(point)) break;
+    if (point >= 80) {
+      rows.push({ point, grade: 'A' });
+    } else if (point >= 70 && point < 80) {
+      rows.push({ point, grade: 'B' });
+    } else if (point >= 60 && point < 70) {
+      rows.push({ point, grade: 'C' });
+    } else if (point >= 50 && point < 60) {
+      rows.push({ point, grade: 'D' });
+    } else {
+      rows.push({ point, grade: 'F' });
+    }
+  }
+
+  const handleDelete = e => {
+    e.preventDefault();
+    let row = e.target.parentNode.parentElement;
+    row.remove();
+  };
+
+  const renderTable = () => {
+    return rows.map((row, idx) => {
+      let { point, grade } = row;
+      return (
+        <tr key={idx}>
+          <td>{point}</td>
+          <td>{grade}</td>
+          <td>
+            <button onClick={handleDelete}>DELETE</button>
+          </td>
+        </tr>
+      );
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Score Table</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Your point</th>
+            <th>Your grade</th>
+          </tr>
+        </thead>
+        <tbody>{renderTable()}</tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/App2.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+class App2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = null;
+  }
+  render() {
+    let point;
+    let rows = [];
+
+    while (true) {
+      point = Number(prompt('Enter your point'));
+      if (isNaN(point)) break;
+      if (point >= 80) {
+        rows.push({ point, grade: 'A' });
+      } else if (point >= 70 && point < 80) {
+        rows.push({ point, grade: 'B' });
+      } else if (point >= 60 && point < 70) {
+        rows.push({ point, grade: 'C' });
+      } else if (point >= 50 && point < 60) {
+        rows.push({ point, grade: 'D' });
+      } else {
+        rows.push({ point, grade: 'F' });
+      }
+    }
+
+    const handleDelete = e => {
+      e.preventDefault();
+      let row = e.target.parentNode.parentElement;
+      row.remove();
+    };
+
+    const renderTable = () => {
+      return rows.map((row, idx) => {
+        let { point, grade } = row;
+        return (
+          <tr key={idx}>
+            <td>{point}</td>
+            <td>{grade}</td>
+            <td>
+              <button onClick={handleDelete}>DELETE</button>
+            </td>
+          </tr>
+        );
+      });
+    };
+    return (
+      <div>
+        <h1>Score Table</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Your point</th>
+              <th>Your grade</th>
+            </tr>
+          </thead>
+          <tbody>{renderTable()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App2;
